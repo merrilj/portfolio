e12edf29275dcff40695705f15fb0a040a21220c
Merril Jeffs <merril@Merrils-MBP.lan>
2017-06-14 15:44:42 -0600

    added home and proj components

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ProjectIndex from './projects/ProjectIndex.js'
+
+export default class Home extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './components/Home.js'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import { Router, Route, browserHistory } from 'react-router'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: '' })
+})
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path='/' component={Home} />
+    </Router>
+  </ApolloProvider>
+),
+  document.getElementById('root')
+)
